616a6af1f8787354a73b09ae61cdde779a5dc378
Christian David Chilatra Mendez <37522628+ChristianChilatra@users.noreply.github.com>
2021-12-03 00:47:01 -0500

    Add files via upload

--- Script/form.js
+++ Script/form.js
@@ -1,4 +1,6 @@
 const $form = document.querySelector('#form')
+const $buttomMenu = document.querySelector('#menu')
+const $sideBar = document.querySelector('#sideBar')
 
 $form.addEventListener('submit', handleSubmit)
 
@@ -15,5 +17,22 @@ async function handleSubmit(event) {
   if (response.ok) {
     $form.reset()
     alert('Gracias por contactarme, pronto respondere')
+  }
+}
+
+$buttomMenu.addEventListener("click", showMenu)
+$sideBar.addEventListener("click", showMenu)
+
+let isVisibility = false
+
+function showMenu(){
+  if (isVisibility) {
+    isVisibility = false
+    console.log(isVisibility);
+    $sideBar.style.top = "-50rem"
+  }else{
+    console.log(isVisibility);
+    isVisibility = true
+    $sideBar.style.top = "30rem"
   }
 }
